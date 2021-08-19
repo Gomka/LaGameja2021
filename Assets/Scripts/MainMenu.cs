3f775a03c895097f6e4db907f8f3f9730d44587a
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2021-08-19 14:27:07 +0200

    Escena 1: Exterior

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Button playButton;
+
+    private void Start()
+    {
+        playButton.Select();
+    }
+
     public void ExitButton()
     {
         Application.Quit();
