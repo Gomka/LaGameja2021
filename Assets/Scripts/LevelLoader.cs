09b1537d6f5dc47ef037f9d5d832fc5a21e5dc8c
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2021-08-20 17:46:23 +0200

    Position persistency

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    private Transform player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<InputHandler>().gameObject.transform;
+
+        LoadCurrentCoords(SceneManager.GetActiveScene().name);
+    }
 
     public void LoadLevel(string levelName)
     {
@@ -13,8 +21,29 @@ public class LevelLoader : MonoBehaviour
 
     IEnumerator LoadScene(string levelName)
     {
+        SaveCurrentCoords(SceneManager.GetActiveScene().name);
         animator.SetTrigger("FadeIn");
         yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(levelName);
+    }
+
+    private void SaveCurrentCoords(string sceneName)
+    {
+        PlayerPrefs.SetFloat(sceneName + "X", player.position.x);
+        PlayerPrefs.SetFloat(sceneName + "Y", player.position.y);
+        PlayerPrefs.SetFloat(sceneName + "Z", player.position.z);
+    }
+
+    private void LoadCurrentCoords(string sceneName)
+    {
+        if(PlayerPrefs.GetInt(sceneName) == 1)
+        {
+            player.position = new Vector3(PlayerPrefs.GetFloat(sceneName + "X"), PlayerPrefs.GetFloat(sceneName + "Y"), PlayerPrefs.GetFloat(sceneName + "Z"));
+        } 
+        else
+        {
+            PlayerPrefs.SetInt(sceneName, 1);
+        }
+
     }
 }
